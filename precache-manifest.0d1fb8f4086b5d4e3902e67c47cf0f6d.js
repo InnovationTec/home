93483394f114ae0edcea5ac11197719a65838779
Hectorde788 <hectordehoyos053@gmail.com>
2021-05-18 15:13:36 -0500

    Updates

---
+++ precache-manifest.0d1fb8f4086b5d4e3902e67c47cf0f6d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c414b984dc4919a4f9c",
+    "url": "/home/static/css/main.b6140918.chunk.css"
+  },
+  {
+    "revision": "7c414b984dc4919a4f9c",
+    "url": "/home/static/js/main.ef87a1df.chunk.js"
+  },
+  {
+    "revision": "4e1ec8947a6857600002",
+    "url": "/home/static/js/runtime~main.0990ee84.js"
+  },
+  {
+    "revision": "985df0a26e651a270dc3",
+    "url": "/home/static/js/2.fe37e546.chunk.js"
+  },
+  {
+    "revision": "44178e47a8fa71ce15388937bdcfd208",
+    "url": "/home/static/media/marketing.44178e47.jpg"
+  },
+  {
+    "revision": "0ed2cfff917cc0d8147d4d84c37c3817",
+    "url": "/home/static/media/diseñog.0ed2cfff.jpg"
+  },
+  {
+    "revision": "af38746f4a45a80bf35832b3e6182cbf",
+    "url": "/home/static/media/desarrollo.af38746f.jpg"
+  },
+  {
+    "revision": "4198e418a8c2ef6a03ad5f890076b7c5",
+    "url": "/home/static/media/header1.4198e418.jpg"
+  },
+  {
+    "revision": "c9eaef12f33ebaab293788ebbc613aa7",
+    "url": "/home/static/media/propuestaIT.c9eaef12.png"
+  },
+  {
+    "revision": "e6bc6d1f57e9116c3017bb108275a31f",
+    "url": "/home/static/media/Proto.e6bc6d1f.png"
+  },
+  {
+    "revision": "feaf2fc2203496aad11ce41d3bbb3c3f",
+    "url": "/home/index.html"
+  }
+];
